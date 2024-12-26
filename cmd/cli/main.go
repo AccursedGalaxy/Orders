67f499c822284e7be35840ac457c65f18576224d
Robin Bohrer <robinbohrer7@gmail.com>
2024-12-26 02:44:20 +0100

    feat: cli update

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"binance-redis-streamer/pkg/cli"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+
 	if err := cli.NewRootCmd().Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
